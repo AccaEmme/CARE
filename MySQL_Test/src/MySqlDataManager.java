f82c4ee09af8d127070fc76d0fe9bb63c9b08418
follen99 <giulianoranauroiphone1@gmail.com>
2021-04-21 17:54:31 +0200

    programma base *non funzionante al momento*

---
+++ MySQL_Test/src/MySqlDataManager.java
@@ -0,0 +1,58 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
+public class MySqlDataManager implements DataManager{
+    private String host;
+    private String db;
+
+    private static final  String username = "root";
+    private static final String password = "to write";
+
+    private static final String SQL_CREATE_DB = "CREATE DATABASE IF NOT EXISTS";
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS SACCHE"
+            + "("
+            + " SERIALE varchar(20) NOT NULL,"
+            + " GRUPPO varchar(5) NOT NULL,"
+            + " PRIMARY KEY (SERIALE)"
+            + ")";
+
+    private static final String SQL_INSERT = "INSERT INTO SACCHE (SERIALE, GRUPPO) VALUES (?,?)";
+
+    private static final String SQL_QUERY = "SELECT * FROM sacche WHERE GRUPPO = ?";
+
+    public MySqlDataManager(){
+        Properties loadProps = new Properties();
+
+        try {
+            loadProps.loadFromXML(new FileInputStream("localsettings/db_settings.xml")); //non dimenticare di aggiungere il file mostrato dal prof per i settings
+
+        } catch (InvalidPropertiesFormatException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createDB(){
+        String url = "jdbc:mysql://"+host;
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+        PreparedStatement preparedStatement = conn.prepareStatement(SQL_CREATE_DB+db)){
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
