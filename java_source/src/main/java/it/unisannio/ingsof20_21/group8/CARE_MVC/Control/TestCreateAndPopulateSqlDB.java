2857c783b7f3a01a7a570c8c692f01b1c2652ce8
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-05-13 23:00:52 +0200

    Revert "All classes implemented"
    This reverts commit 1cb951cd81ee6102bbe27985ba2d34352358bb8f.

---
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Control/TestCreateAndPopulateSqlDB.java
@@ -0,0 +1,37 @@
+package it.unisannio.ingsof20_21.group8.CARE_MVC.Control;
+
+import java.util.Random;
+
+import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Blood.Blood;
+import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Blood.BloodBag;
+import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Managers.DataManager;
+import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Managers.MySqlDataManager;
+
+
+public class TestCreateAndPopulateSqlDB {
+
+	public static void main(String[] args) {
+		DataManager dm = new MySqlDataManager();
+		dm.createDB();
+		
+		final int NUMSACCHE=15;
+	    Blood gruppi[] = Blood.values();
+		Random generatore = new Random();
+		BloodBag s;
+		for (int i=0; i<NUMSACCHE; i++) {
+			s = new BloodBag(gruppi[generatore.nextInt(gruppi.length)]);
+			dm.addBloodBag(s);
+			System.out.println("Added BloodBag: "+s);			
+		}		
+		
+		/*
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+		    public void run() {
+		        Serial.updateSettings();
+		    }
+		}));
+		*/
+
+	}
+
+}
