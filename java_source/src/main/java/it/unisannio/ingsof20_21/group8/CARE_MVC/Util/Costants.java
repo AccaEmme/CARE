336e95caa64191c48ef3e12c1bba38803f21d445
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-05-13 21:34:21 +0200

    Utils

---
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Util/Costants.java
@@ -0,0 +1,17 @@
+/**
+ * @author giuliano ranauro
+ * Date: 13/05/2021 21:32
+ * Ide: Intellij
+ * JDK: 1.8
+ * @version 1.0
+ */
+package it.unisannio.ingsof20_21.group8.CARE_MVC.Util;
+
+public class Costants {
+    public static final String DATE_FORMAT = "yyyy-mm-dd_hh-mm-ss";
+    public static final String SERIAL = "SERIAL";
+    public static final String GROUP = "GROUP";
+    public static final String EXPIRATION = "EXPIRATION_DAY";
+    public static final String CREATION = "CREATION_DAY";
+    public static final String ORIGIN = "ORIGIN";
+}
