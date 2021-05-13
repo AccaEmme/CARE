5be9f0c9f8893e22106c5620551392c1dd63787a
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-05-13 23:00:56 +0200

    Revert "Utils"
    This reverts commit 4a06c3e70b1ba9d93c025bd72bacd8653de911e6.

--- java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Util/DateGenerator.java
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Util/DateGenerator.java
@@ -5,7 +5,7 @@
  * JDK: 1.8
  * @version 1.0
  */
-package it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Util;
+package it.unisannio.ingsof20_21.group8.CARE_MVC.Util;
 
 
 import java.text.ParseException;
