946ba86b2de4fb1bb3cc3cec1b7f58873b32777b
Omni-star <alexcrov99@gmail.com>
2021-05-26 13:01:32 +0200

    Modifiche class 'Location.java' e implementazione degli enum nell'interfaccia 'LocationEnum.java'
    Modifiche alla classe 'Location.java'.

---
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Node/LocationEnum.java
@@ -0,0 +1,21 @@
+package it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Node;
+
+public interface LocationEnum {
+
+	public enum Country {
+		Italia, Spagna, Croazia, Grecia;
+	}
+	
+	public enum Region {
+		Abruzzo, Basilicata, Bolzano, Calabria, Campania, Emilia_Romagna, Friuli_Venezia_Giulia, Lazio,	Liguria, Lombardia,	Marche,	Molise, Piemonte, Puglia, Sardegna, Sicilia, Toscana, Trento, Umbria, Valle_d_Aosta, Veneto;
+		}
+	
+	public enum Province {
+		
+		Chieti, Potenza, Bolzano, Catanzaro, Napoli, Bologna, Udine, Roma, Genova, Varese, Ancona, Campobasso, Torino, Bari, Cagliari, Palermo, Firenze, Trento, Perugia, Aosta, Padova, Benevento, Avellino, Salerno, Caserta;
+	}
+	
+	public enum City {
+		Chieti, Potenza, Bolzano, Catanzaro, Napoli, Bologna, Udine, Roma, Genova, Varese, Ancona, Campobasso, Torino, Bari, Cagliari, Palermo, Firenze, Trento, Perugia, Aosta, Padova, Benevento, Avellino, Salerno, Caserta;
+	}
+}
