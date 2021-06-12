2500b59ed57bbbbfd4d9fe8f9ed64271e2b26ebd
R-luigi99 <80921217+R-luigi99@users.noreply.github.com>
2021-06-12 18:41:16 +0200

    wdw

--- Garbage4Test/soluzioneLuigi/src/main/java/it/gc/Prj2021Pillola04/persistence/ReportAdministrator.java
+++
@@ -1,7 +0,0 @@
-package it.gc.Prj2021Pillola04.persistence;
-
-public interface ReportAdministrator extends DataManager{
-
-	/*report ecc...*/
-	public void reportGiacenze();
-	}
--- Garbage4Test/soluzioneLuigi/src/main/java/users/Role.java
+++
@@ -1,9 +0,0 @@
-package users;
-
-/*
- * https://github.com/AccaEmme/CARE/wiki/5.3-User-authentication-privileges-and-accesses-to-resources
- */
-
-public enum Role {
-	Administrator, StoreManager, Officer;
-}
