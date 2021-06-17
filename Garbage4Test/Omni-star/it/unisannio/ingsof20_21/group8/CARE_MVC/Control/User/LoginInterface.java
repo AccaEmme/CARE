c1339b3b6b8729f5d899dd121b9e4ed93f793174
Omni-star <alexcrov99@gmail.com>
2021-06-18 01:19:52 +0200

    Ordine

--- Garbage4Test/Omni-star/it/unisannio/ingsof20_21/group8/CARE_MVC/Control/User/LoginInterface.java
+++
@@ -1,7 +0,0 @@
-package it.unisannio.ingsof20_21.group8.CARE_MVC.Control.User;
-
-import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.User.User;
-
-public interface LoginInterface {
-	public User getUser();
-}
