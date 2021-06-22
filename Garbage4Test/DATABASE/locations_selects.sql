9b785e15dfc13d0311f3b2bf694fc02c7db9b53a
follen99 <giulianoranauroiphone1@gmail.com>
2021-06-22 23:48:28 +0200

    update query

--- Garbage4Test/DATABASE/locations_selects.sql
+++ Garbage4Test/DATABASE/locations_selects.sql
@@ -16,3 +16,21 @@ select count(*)
 		where 	r.nome = "Campania" 	and
 				p.nome = "Benevento" 	and
                 c.nome = "Apice";
+
+-- controlla se una determinata tupla esiste
+-- ritorna le componenti per creare un oggetto (da creare in java) location
+
+select r.nome as "region", p.nome as "province", c.nome as "city"
+	from regioni r join province p join comuni c
+    on 		c.id_provincia = p.id and p.id_regione = r.id
+		where 	r.nome = "Campania" 	and
+				p.nome = "Benevento" 	and
+                c.nome = "Apice";
+                
+-- ritorna la posizione di una città specifica
+select c.nome as "nome comune", c.latitudine as "latitudine", c.longitudine as "longitudine"
+	from regioni r join province p join comuni c
+		on 		c.id_provincia = p.id and p.id_regione = r.id
+			where 	r.nome = "Campania" 	and
+					p.nome = "Benevento" 	and
+					c.nome = "Apice";
