3861b70f6e904dd0ea7d99d3c54de594bd4e5f1b
DonyBerserker <donato.polvere@yahoo.it>
2021-06-24 17:06:28 +0200

    Update Role con Javadocs

--- java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/User/Role.java
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/User/Role.java
@@ -4,6 +4,11 @@ package it.unisannio.ingsof20_21.group8.CARE_MVC.Model.User;
  * https://github.com/AccaEmme/CARE/wiki/5.3-User-authentication-privileges-and-accesses-to-resources
  */
 
+/**
+**************************************************************************
+ * Enum con tutti i ruoli che possono avere gli utenti
+ **************************************************************************
+*/
 public enum Role {
 	Administrator, StoreManager, Officer;
 }
