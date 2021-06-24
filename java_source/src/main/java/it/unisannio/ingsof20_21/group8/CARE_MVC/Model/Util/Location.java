24e2587371e98a4123399e4abf4cb9542e40e168
DonyBerserker <donato.polvere@yahoo.it>
2021-06-24 16:47:29 +0200

    Update v2 Location

--- java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Util/Location.java
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Util/Location.java
@@ -1,11 +1,6 @@
 package it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Util;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
-import it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Blood.BloodBag;
 import org.bson.Document;
-
 
 /*
  * Hermann: no, questa classe che potrebbe anche essere astratta, definisce una locazione, può essere dov'è situata una sacca o la città di un utente.
@@ -43,6 +38,13 @@ public class Location {
         this.country = country;
     }
     
+     /**
+     **************************************************************************
+     * Descrizione del metodo
+     * @param 
+     * @return 
+     **************************************************************************
+     */
     public Document getDocument(){
     	Document document = new Document();
     		document.append("street",		this.street.toString());
@@ -67,42 +69,128 @@ public class Location {
 	}
 	*/
 
-
     
+
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere la strada
+     * @return stret
+     **************************************************************************
+     */
     public String getStreet() { return street; }
     
+     /**
+     **************************************************************************
+     * Metodo Get per ottenere il numero della strada 
+     * @return streetNumber
+    **************************************************************************
+     */
     public String getStreetNumber() { return streetNumber; }
     
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere la città
+    * @return city
+     **************************************************************************
+     */
     public City getCity() { return city; }
     
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere la città
+     * @return city
+     **************************************************************************
+     */  
     public Province getProvince() { return province; }
     
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere la città
+     * @return city
+     **************************************************************************
+     */
     public Region getRegion() { return region; }
     
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere la nazione
+     * @return country
+     **************************************************************************
+     */
     public Country getCountry() { return country; }
     
+    /**
+     **************************************************************************
+     * Metodo Get per ottenere lo ZIPCode
+     * @return ZIPCode
+     **************************************************************************
+     */
     public String getZipCode() { return ZIPCode; }
     
+    
+    
+    /**
+     **************************************************************************
+     * Metodo Set per inserire una nuova strada
+     * @param streetR
+     **************************************************************************
+     */
     public void setStreet(String streetR) throws IllegalArgumentException{ 
 		if( streetR.equals("") )
 			throw new IllegalArgumentException("Il nome della strada non è stato inserito");
 		this.street = streetR;
 	}
-
+    
+    /**
+     **************************************************************************
+     * Metodo Set per inserire un nuovo numero della strada
+     * @param streetNumberR
+     **************************************************************************
+     */
     public void setStreetNumber(String streetNumberR) throws IllegalArgumentException{
 		if( streetNumberR.equals("") ) 
 			throw new IllegalArgumentException("Il numero della strada non è stato inserito");
 		this.streetNumber  = streetNumberR; 
 	}
-    
+   
+    /**
+     **************************************************************************
+     * Metodo Set per inserire una nuova città
+     * @param city
+     **************************************************************************
+     */
     public void setCity(String cityR) { city = City.valueOf(cityR); }
     
+    /**
+     **************************************************************************
+     * Metodo Set per inserire una nuova provincia
+     * @param provinceR
+     **************************************************************************
+     */
     public void setProvince(String provinceR) { province = Province.valueOf(provinceR); }
     
+    /**
+     **************************************************************************
+     * Metodo Set per inserire una nuova regione
+     * @param regionR
+     **************************************************************************
+     */
     public void setRegion(String regionR) { region = Region.valueOf(regionR); }
     
+    /**
+     **************************************************************************
+     * Metodo Set per inserire una nuova nazione
+     * @param countryR
+     **************************************************************************
+     */
     public void setCountry(String countryR) { country = Country.valueOf(countryR); }
     
+    /**
+     **************************************************************************
+     * Metodo Set per inserire un nuovo zipcode
+     * @param ZIPcode
+     **************************************************************************
+     */
     public void setZipCode(String ZIPcode) { 
     	if( ZIPcode.equals("") ) 
 			throw new IllegalArgumentException("Il numero zipcode non è valido");
@@ -110,6 +198,12 @@ public class Location {
     }
     
     
+    /**
+     **************************************************************************
+     * Metodo ToString per ottenere tutte le informazioni registrate nella classe Location
+     * @return (Street, StreetNumber, City, Province, Region, Country, ZIPcode)
+     **************************************************************************
+     */
 	public String toString() {
 		return  "{\"street\": \"" 	  		+ this.street   		+ "\""
 				+", \"streetNumber\": \""  	+ this.streetNumber 	+ "\"" 
@@ -120,22 +214,49 @@ public class Location {
 				+", \"ZIPCode\": \"" 		+ this.ZIPCode 			+ "\""
 				+ "}";
 	}
-    
+	
+	/**
+	 **************************************************************************
+	 * Metodo Enum per registrare tutte le informazioni della Nazione
+	 **************************************************************************
+	 */
 	public enum Country {
 		Italy, Spain, Croatia, Greece;
 	}
 	
+	/**
+	 **************************************************************************
+	 * Metodo Enum per registrare tutte le informazioni della Regione
+	 **************************************************************************
+	 */
+	
 	public enum Region {
 		Abruzzo, Basilicata, Bolzano, Calabria, Campania, Emilia_Romagna, Friuli_Venezia_Giulia, Lazio,	Liguria, Lombardia,	Marche,	Molise, Piemonte, Puglia, Sardegna, Sicilia, Toscana, Trento, Umbria, Valle_d_Aosta, Veneto;
 		}
+	/**
+	 **************************************************************************
+	 * Metodo Enum per registrare tutte le informazioni della provincia
+	 **************************************************************************
+	 */
 	
 	public enum Province {
 		Chieti, Potenza, Bolzano, Catanzaro, Napoli, Bologna, Udine, Roma, Genova, Varese, Ancona, Campobasso, Torino, Bari, Cagliari, Palermo, Firenze, Trento, Perugia, Aosta, Padova, Benevento, Avellino, Salerno, Caserta;
 	}
+	/**
+	 **************************************************************************
+	 * Metodo Enum per registrare tutte le informazioni della Città
+	 **************************************************************************
+	 */
 	
 	public enum City {
 		Chieti, Potenza, Bolzano, Catanzaro, Napoli, Bologna, Udine, Roma, Genova, Varese, Ancona, Campobasso, Torino, Bari, Cagliari, Palermo, Firenze, Trento, Perugia, Aosta, Padova, Benevento, Avellino, Salerno, Caserta;
 	}
-    
+	
+	/**
+	 **************************************************************************
+	 * Metodo per il tester Junit per verificare l'esistenza dell'oggetto Location
+	 * @return true
+	 **************************************************************************
+	 */
 	public boolean exists() { return true; }
 }
