bee87dc4b2960e446ae422846b52602c9c036dc0
DonyBerserker <donato.polvere@yahoo.it>
2021-06-24 17:55:02 +0200

    Update Javadocs nodi

--- java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Node/Node.java
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Node/Node.java
@@ -14,6 +14,14 @@ public class Node {
 	private TreeMap<BloodGroup, ArrayList<Integer>> quantities;
 	private TreeMap<BloodGroup, ArrayList<Integer>> temp_quantities;
 	
+	
+	/**
+     **************************************************************************
+     * Metodo Costruttore per la creazione del Nodo
+     * @param String codStr, String nodeName, Location warehouse
+     **************************************************************************
+     */
+	
 	public Node(String codStr, String nodeName, Location warehouse) {
 		this.setCodStr(codStr);
 		this.setNodeName(nodeName);
@@ -23,32 +31,75 @@ public class Node {
 		this.temp_quantities = new TreeMap<BloodGroup, ArrayList<Integer>>(); 
 	}
 	
+	/**
+     **************************************************************************
+     * Metodo GET per ottenere il codice struttura
+     * @return codStr
+     **************************************************************************
+     */
 	public String getCodStr() {
 		return codStr;
 	}
 	
+	/**
+     **************************************************************************
+     * Metodo SET per inserire un nuovo il codice struttura
+     * @param String codStr
+     * @exception IllegalArgumentException
+     **************************************************************************
+     */
 	private void setCodStr(String codStr) {
 		if( codStr.length()>3 )	throw new IllegalArgumentException("Node.java - codStr length can't be over three chars");
 		this.codStr = codStr;
 	}
 
+	/**
+     **************************************************************************
+     * Metodo GET per ottenere il nome del noto
+     * @return nodeName
+     **************************************************************************
+     */
 	public String getNodeName() {
 		return nodeName;
 	}	
 
+	/**
+     **************************************************************************
+     * Metodo SET per inserire il nuovo nome di un nodo
+     * @param String nodeName
+     **************************************************************************
+     */
 	private void setNodeName(String nodeName) {
 		this.nodeName = nodeName;
 	}
 
-	
+	/**
+     **************************************************************************
+     * Metodo GET per ottenere la posizione del nodo
+     * @return warehouse
+     **************************************************************************
+     */
 	public Location getWarehouse() {
 		return warehouse;
 	}
-
+	
+	/**
+     **************************************************************************
+     * Metodo GET per inserire la posizione del nodo
+     * @param String warehouse
+     **************************************************************************
+     */
 	private void setWarehouse(Location warehouse) {
 		this.warehouse = warehouse;
 	}
 	
+	/**
+     **************************************************************************
+     * Metodo per la verificare che due nodi siano uguali
+     * @param Object obj
+     * @return codStr.equals( n.getCodStr() )
+     **************************************************************************
+     */
 	public boolean equals(Object obj) {
 		if(obj instanceof BloodBag) {
 			Node n = (Node) obj;
@@ -57,12 +108,24 @@ public class Node {
 		return false;
 	}
 
+	/**
+     **************************************************************************
+     * Metodo per il ritorno di tutti i dati del nodo
+     * @return codStr, NodeName, warehouse.toString()
+     **************************************************************************
+     */
 	public String toString() {
 		return   "{\"codStr\": \"" 	  	+ this.codStr   			  + "\""
 				+", \"nodeName\": \""  	+ this.nodeName 			  + "\"" 
 				+", \"wareHouse\": " 	+ this.warehouse.toString()   + "}";
 	}
 	
+	/**
+     **************************************************************************
+     * Metodo per i limiti di immagazzinamento sacche di ogni nodo
+     * @param BloodGroup bg, int min, int max
+     **************************************************************************
+     */
 	public void setAllowedQuantity(BloodGroup bg, int min, int max) {
 		ArrayList<Integer> bloodgroupLimits = new ArrayList<Integer>();
 		bloodgroupLimits.add(min);
@@ -73,6 +136,12 @@ public class Node {
 	}
 	
 	
+	/**
+     **************************************************************************
+     * Metodo per definisce le quantità minime e massime di sacche di sangue per ogni nodo
+     * @param TreeMap<BloodGroup, ArrayList<Integer>> quant
+     **************************************************************************
+     */
 	private void setAllowedQuantities(TreeMap<BloodGroup, ArrayList<Integer>> quant) {
 		/* Defines the minimum and maximum bloodbags quantities for each node.
 		 * Limits exists only if specified.
@@ -92,6 +161,12 @@ public class Node {
 		this.quantities=quant;
 	}
 	
+	/**
+     **************************************************************************
+     * Metodo per visualizzare il minimo ed il massimo delle quantità di sacche di sangue 
+     * @param PrintStream output
+     **************************************************************************
+     */
 	public void printAllowedQuantities(PrintStream output/*, TreeMap<BloodGroup, ArrayList<Integer>> quant*/) {
 		int min, max, firstvalue=1;
 		output.println("[");
