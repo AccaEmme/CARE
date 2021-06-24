e86608f234ef165e07f092d1b334eb89d471aaaf
DonyBerserker <donato.polvere@yahoo.it>
2021-06-24 17:40:41 +0200

    Update RequestState con javadocs

--- java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Request/RequestState.java
+++ java_source/src/main/java/it/unisannio/ingsof20_21/group8/CARE_MVC/Model/Request/RequestState.java
@@ -1,5 +1,10 @@
 package it.unisannio.ingsof20_21.group8.CARE_MVC.Model.Request;
 
+/**
+ **************************************************************************
+ * Metodo ENUM per i vari stati della richiesta
+ **************************************************************************
+ */
 public enum RequestState {
  accepted,refused,pending;
 }
