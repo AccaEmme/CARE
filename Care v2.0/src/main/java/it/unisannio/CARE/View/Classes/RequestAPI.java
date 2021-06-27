38cd189346a20460640f6921a66146a4297f6605
R-luigi99 <80921217+R-luigi99@users.noreply.github.com>
2021-06-27 16:43:13 +0200

    @rest

--- Care v2.0/src/main/java/it/unisannio/CARE/View/Classes/RequestAPI.java
+++ Care v2.0/src/main/java/it/unisannio/CARE/View/Classes/RequestAPI.java
@@ -1,6 +1,9 @@
 package it.unisannio.CARE.View.Classes;
 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class RequestAPI {
+
 
 	public RequestAPI() {
 		// TODO Auto-generated constructor stub
