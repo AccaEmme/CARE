aabc5a10fd80454a00cd9dcba2f5d6327d9ce2c2
follen99 <giulianoranauroiphone1@gmail.com>
2021-07-02 16:23:30 +0200

    classe qr code con dipendenze some junit tests

--- Care_v2.0/src/main/java/it/unisannio/CARE/model/util/QRCode.java
+++ Care_v2.0/src/main/java/it/unisannio/CARE/model/util/QRCode.java
@@ -1,5 +1,107 @@
 package it.unisannio.CARE.model.util;
 
+import it.unisannio.CARE.model.bloodBag.BloodBag;
+import it.unisannio.ingsof20_21.group8.Care.Spring.BloodBagDAO;
+import org.json.simple.JSONObject;
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 public class QRCode {
+    String object;
+    String identifier;
+
+
+    public QRCode(JSONObject objectToWrite){
+        this.object = objectToWrite.toJSONString();
+        this.identifier = objectToWrite.get("serial").toString();
+    }
+    public QRCode(String objectToWrite, String identifier){
+        this.object = objectToWrite;
+        this.identifier = identifier;
+    }
+    public QRCode(BloodBag objectToWrite){
+        this.object = this.createBloodBagObject(objectToWrite).toJSONString();
+        this.identifier = objectToWrite.getSerial().toString();
+    }
+
+    public QRCode(BloodBagDAO objectToWrite){
+        this.object = this.createBloodBagObject(objectToWrite).toJSONString();
+        this.identifier = objectToWrite.getSerial();
+    }
+
+
+
+
+
+
+    private JSONObject createBloodBagObject(BloodBag bag){
+        JSONObject object = new JSONObject();
+            object.put("serial",bag.getSerial());
+            object.put("group",bag.getBloodGroup().toString());
+            object.put("creationDate",bag.getCreationDate());
+            object.put("expirationDate",bag.getExpirationDate());
+            object.put("donator",bag.getDonatorCF());
+            object.put("state",bag.getBloodBagState());
+            object.put("notes",bag.getNote());
+
+        return object;
+    }
+    private JSONObject createBloodBagObject(BloodBagDAO bag){
+        JSONObject object = new JSONObject();
+        object.put("serial",bag.getSerial());
+        object.put("group",bag.getGroup());
+        object.put("creationDate",bag.getCreationDate());
+        object.put("expirationDate",bag.getExpirationDate());
+        object.put("donator",bag.getDonator());
+        object.put("state",bag.getState());
+        object.put("notes",bag.getNotes());
+
+        return object;
+    }
+
+
+    //important stuff
+    public void createQRCode(){
+        try {
+            String qrCodeData = object;
+            String filePath = "QR_codes/"+identifier+"_"+new Date().getTime() +".png";
+            String charset = "UTF-8"; // or "ISO-8859-1"
+            Map < EncodeHintType, ErrorCorrectionLevel > hintMap = new HashMap < EncodeHintType, ErrorCorrectionLevel > ();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            BitMatrix matrix = new MultiFormatWriter().encode(
+                    new String(qrCodeData.getBytes(charset), charset),
+                    BarcodeFormat.QR_CODE, 200, 200, hintMap);
+            MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
+                    .lastIndexOf('.') + 1), new File(filePath));
+            System.out.println("QR Code image created successfully!");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
 
 }
---
+++ Care_v2.0/src/test/java/it/unisannio/CARE/spring/QRCodeTester.java
@@ -0,0 +1,77 @@
+package it.unisannio.CARE.spring;
+
+import it.unisannio.CARE.model.bloodBag.BloodBag;
+import it.unisannio.CARE.model.util.QRCode;
+import it.unisannio.ingsof20_21.group8.Care.Spring.BloodBagDAO;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+public class QRCodeTester {
+    @Test
+    public void testCreateQRFromJSONObject(){
+        /**
+         * {
+         *  "serial":"IT-NA205101-Aneg-20210615-0037",
+         *  "group":"Bneg",
+         *  "creationDate":965837967,
+         *  "expirationDate":965837968,
+         *  "donator":"CRSDLCER86BH0919",
+         *  "state":"Available",
+         *  "notes":"test note"
+         * }*/
+        BloodBagDAO bag = new BloodBagDAO();
+            bag.setSerial("IT-NA205101-Aneg-20210615-0037");
+            bag.setGroup("Bneg");
+            bag.setCreationDate(965837967);
+            bag.setExpirationDate(965837968);
+            bag.setDonator("CRSDLCER86BH0919");
+            bag.setState("Available");
+            bag.setNotes("buonanott");
+
+    JSONObject object = new JSONObject();
+        object.put("serial",bag.getSerial());
+        object.put("group",bag.getGroup());
+        object.put("creationDate",bag.getCreationDate());
+        object.put("expirationDate",bag.getExpirationDate());
+        object.put("donator",bag.getDonator());
+        object.put("state",bag.getState());
+        object.put("notes",bag.getNotes());
+
+        QRCode code = new QRCode(object);
+
+        code.createQRCode();
+    }
+
+    @Test
+    public void testCreateQRCodeFromBloodBag(){
+        BloodBagDAO bag = new BloodBagDAO();
+        bag.setSerial("IT-NA205101-Aneg-20210615-0037");
+        bag.setGroup("Bneg");
+        bag.setCreationDate(965837967);
+        bag.setExpirationDate(965837968);
+        bag.setDonator("CRSDLCER86BH0919");
+        bag.setState("Available");
+        bag.setNotes("buonanott");
+
+        QRCode code = new QRCode(bag);
+        code.createQRCode();
+    }
+
+    @Test
+    public void testCreateQRCodeFromString(){
+        String qr = "Peppiniello è incazzato";
+        String id = "peppiniello_importunato";
+
+        QRCode code = new QRCode(qr,id);
+        code.createQRCode();
+    }
+
+    @Test
+    public void testCreateQRCodeFromStringException(){
+        String qr = "Peppiniello è incazzato";
+        String id = "peppiniello_importunato";
+
+        QRCode code = new QRCode(qr,id);
+        code.createQRCode();
+    }
+}
