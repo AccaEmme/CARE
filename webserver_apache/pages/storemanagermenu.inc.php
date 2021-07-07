78445780181f51c90a7b765b788a7df0648cb3ed
AccaEmme <acca.m@hotmail.it>
2021-07-07 09:32:02 +0200

    added logout to all menu pages

--- webserver_apache/pages/storemanagermenu.inc.php
+++ webserver_apache/pages/storemanagermenu.inc.php
@@ -1,3 +1,4 @@
 <?php $currenturl='http://'.apache_getenv("HTTP_HOST") . apache_getenv("REQUEST_URI"); ?>
        <img src="images\magazzino.png" weight="3%" height="3%" /><a href="<?php echo($currenturl . '&subpage=storemanagerdashboard'); ?>">In Magazzino</a><br />
-       <img src="images\scadenza.png" weight="3%" height="3%" /><a href="<?php echo($currenturl . '&subpage=storemanager_requests'); ?>">Richieste</a><br />       
+       <img src="images\scadenza.png" weight="3%" height="3%" /><a href="<?php echo($currenturl . '&subpage=storemanager_requests'); ?>">Richieste</a><br />
+       <img src="images\scadenza.png" weight="3%" height="3%" /><a href="./logout.php">Logout (<?php echo($username); ?>)</a><br />
