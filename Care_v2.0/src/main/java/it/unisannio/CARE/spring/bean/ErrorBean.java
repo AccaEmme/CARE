8cae4eb30ae0ffded1a4f4bd348ea45fa56c51cb
DonyBerserker <donato.polvere@yahoo.it>
2021-07-08 12:47:05 +0200

    update Javadocs

--- Care_v2.0/src/main/java/it/unisannio/CARE/spring/bean/ErrorBean.java
+++ Care_v2.0/src/main/java/it/unisannio/CARE/spring/bean/ErrorBean.java
@@ -1,13 +1,22 @@
 package it.unisannio.CARE.spring.bean;
 
-
+/**
+ * 
+ */
 
 public class ErrorBean {
 
     private String timestamp, status, error, description, path;
     
     
-
+/**
+ *  Constructor method of the ErrorBean class
+ * @param timestamp timestamp of the error
+  * @param status status of the error
+  * @param error message type
+  * @param description description of the error
+  * @param path path of the error
+ */
 	public ErrorBean(String timestamp, String status, String error, String description, String path) {
 		
 		this.timestamp = timestamp;
@@ -18,51 +27,86 @@ public class ErrorBean {
 	}
 
 	
-	
+	/**
+	 * GET method of timeStamp
+	 * @return returns the timeStamp
+	 */
 	public String getTimestamp() {
 		return timestamp;
 	}
-
+	
+	/**
+	 * Set method of the time stamp
+	 * @param timestamp string variable for inserting the time stamp
+	 */
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
 
 	
-	
+	/**
+	 * GET method for status return
+	 * @return returns the status in string format
+	 */
 	public String getStatus() {
 		return status;
 	}
-
+	
+	/**
+	 * SET method of status
+	 * @param status variable String for entering the status
+	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
 	
-	
+	/**
+	 * GET method for error return
+	 * @return returns the error
+	 */
 	public String getError() {
 		return error;
 	}
 
+	/**
+	 * SET method to insert the error
+	 * @param error variable string of the error
+	 */
 	public void setError(String error) {
 		this.error = error;
 	}
 
 	
-	
+	/**
+	 * GET method to get the description
+	 * @return returns the description
+	 */
 	public String getDescription() {
 		return description;
 	}
-
+	
+	/**
+	 * SET method to enter the description
+	 * @param description variable string of the description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
 	
-	
+	/**
+	 * GET method to get the Path
+	 * @return returns the Path of the error
+	 */
 	public String getPath() {
 		return path;
 	}
 
+	/**
+	 * SET method to insert the error
+	 * @param path path in string format of the error
+	 */
 	public void setPath(String path) {
 		this.path = path;
 	}
